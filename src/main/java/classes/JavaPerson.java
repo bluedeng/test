1993230728116a722c2108ae893d9d024eb43b02
dengtianzhi <dengtianzhi@meituan.com>
2017-05-12 11:48:35 +0800

    tmp

--- src/main/java/classes/JavaPerson.java
+++ src/main/java/classes/JavaPerson.java
@@ -6,7 +6,7 @@ package classes;// Copyright (C) 2016 Meituan
  * @version 1.0
  * @created 2016/10/11 下午3:46
  */
-public class JavaPersion {
+public class JavaPerson {
 
     private String name;
 
@@ -31,5 +31,9 @@ public class JavaPersion {
     @Override
     public String toString() {
         return "JavaPersion{" + "name='" + name + '\'' + ", age=" + age + '}';
+    }
+
+    public static void main(String... args) {
+        System.out.println(new JavaPerson().toString());
     }
 }
--- src/main/scala/ScalaMain.scala
+++ src/main/scala/ScalaMain.scala
@@ -1,6 +1,3 @@
-import classes.{JavaEmail, JavaPersion, ScalaPerson}
-import Utils.BasicUtils
-import objects.BasicCalUtils
 
 /**
   * Created by bluedeng on 2016/10/11.
