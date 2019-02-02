eebdad84bec485f64d49e1c41d9c563f72089e0f
dengtianzhi <dengtianzhi@meituan.com>
2019-02-02 17:37:35 +0800

    抽奖场景：库存分布即抽中概率分布

---
+++ src/main/java/classes/lottery/LotteryWhenStockStandAloneProbability.java
@@ -0,0 +1,13 @@
+package classes.lottery;
+
+/**
+ * @author dengtianzhi
+ * @version 1.0
+ * @created 2019/2/2.
+ */
+public class LotteryWhenStockStandAloneProbability {
+
+    public static void main(String... args) {
+        //
+    }
+}
